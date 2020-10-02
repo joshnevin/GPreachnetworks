0447485a61636c5f3e8b4bb5db9aa7d0bc65d737
Josh Nevin <jn399@cam.ac.uk>
2020-10-02 11:59:46 +0100

    adapted HGP to fit to MS dataset

--- HGP.py
+++ HGP.py
@@ -24,15 +24,109 @@ from scipy.stats import norm
 #import matplotlib
 import math
 #matplotlib.rc_file_defaults()   # use to return to Matplotlib defaults 
+import pandas as pd
+def get_list_simulations(root_dir):
+    """
+    Get a list with all the name of the simulation csv files.
+    """
+    file_list_simulations = []
+    for _, file in enumerate(os.listdir(root_dir)):
+        file_list_simulations.append(file)
+    return file_list_simulations
+
+def get_dict_scenario_txt(file_list_simulations):
+    """
+    Build dictionary of scenario number and the corresponding name of the csv file
+    with simulation data.
+    """
+    simulation_files_dict = {}
+    for file_name in file_list_simulations:
+        if file_name.endswith('.docx'):
+            continue
+        else:
+            #simulation_files_dict[str(file_name.split("_")[1]) + '_' + str(file_name.split("_")[3].rstrip('.txt'))] = file_name
+            simulation_files_dict[str(file_name.split("_")[1])] = file_name
+    return simulation_files_dict
+
+def down_sample(df, sam):
+    """
+    downsample to every hour and return Q and CD arrays 
+    """
+    sam = int(sam)
+    Qarr = df["Q-factor"].to_numpy()
+    CDarr = df["CD"].to_numpy()
+    PMDarr = df["PMD"].to_numpy()
+    Qarr = [Qarr[i] for i in range(0, round(len(Qarr)), sam)] 
+    CDarr = [CDarr[i] for i in range(0, round(len(CDarr)), sam)]  
+    PMDarr = [PMDarr[i] for i in range(0, round(len(PMDarr)), sam)] 
+    return Qarr, CDarr, PMDarr
+
+
+def drop_bad_values(CDarr, Qarr):
+    """
+    set Q factor values for which CD is > 2 sigma from the mean to NaN so they are missed from plots
+    """
+    CDsd = np.std(CDarr)
+    CDmean = np.mean(CDarr)
+    Qsd = np.std(Qarr)
+    Qmean = np.mean(Qarr)
+    for i in range(len(CDarr)):
+        if abs(CDarr[i] - CDmean) > 6*CDsd or abs(Qarr[i] - Qmean) > 6*Qsd:
+            Qarr[i] = np.nan
+    return Qarr
+
+def read_txt_to_df(file, root_dir):
+    """
+    read individual channel txt file to a df
+    """
+    file_path = os.path.join(root_dir, file)
+    df = pd.read_csv(file_path, sep='\t', header=[0,1])
+    columns = ["date", "Q-factor", "Power", "CD", "PMD"] 
+    df.columns = columns
+    df = convert_objects_to_float(df)
+    return df
+
+def convert_objects_to_float(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Convert columns with object dtype to floats in order to use them in models.
+    """
+    indicator = df.dtypes == 'object'
+    categorical_columns = df.columns[indicator].tolist()
+    for col in categorical_columns:
+        try:
+            df[col] = df[col].astype('float')
+        except:
+            continue
+    return df
+
+# %%
+channel = '20'
+root_dir = '/Users/joshnevin/Desktop/MicrosoftDataset'
+file_list_simulations= get_list_simulations(root_dir)
+simulation_files_dict = get_dict_scenario_txt(file_list_simulations)
+
+df = read_txt_to_df(simulation_files_dict[channel], root_dir)
+Qarr, CDarr, PMDarr = down_sample(df, 1) # set sam = 1 to just return arrays with same sampling
+Qarr = drop_bad_values(CDarr, Qarr)
+
+downsampleval = 150
+snr = np.asarray([Qarr[i] for i in range(0, round(len(Qarr)), downsampleval)])  # downsample by factor of 10
+snr = snr.reshape(len(snr),1)
+x = np.linspace(0, len(snr), len(snr))
+
+numpoints = len(snr)
+numedges = 1
+# %%
 
 #snr = np.genfromtxt(open("hetdata20.csv", "r"), delimiter=",", dtype =float) # run heteroscedastic datagen section from GPreachringsrand.py 
 #snr = np.genfromtxt(open("hetdataextdegdv.csv", "r"), delimiter=",", dtype =float) # run heteroscedastic datagen section from GPreachringsrand.py 
-snr = np.genfromtxt(open("hetdataextdegst.csv", "r"), delimiter=",", dtype =float) # run heteroscedastic datagen section from GPreachringsrand.py 
-numpoints = np.size(snr,1)
-numedges = np.size(snr,0)
+#snr = np.genfromtxt(open("hetdataextdegst.csv", "r"), delimiter=",", dtype =float) # run heteroscedastic datagen section from GPreachringsrand.py 
+#numpoints = np.size(snr,1)
+#numedges = np.size(snr,0)
 #x = np.linspace(0,numpoints-1,numpoints)
-x = np.linspace(0,1,numpoints)
-# 
+#x = np.linspace(0,1,numpoints)
+
+
 #SNR = SNR[0]
 #snr = snr[0:1]
 # # data used by Goldberg - use for testing 
@@ -66,7 +160,7 @@ sd = 0.01 + 0.25*(1 - np.sin(2.5*x))**2
 y = np.random.normal(wmean, sd)
 snr = y """
 
-def HGPfunc(x,y,plot):
+def HGPfunc(x,y,plot, hlow, hhigh):
     y = y.reshape(-1,1)
     x = x.reshape(-1,1)
     if plot:
@@ -140,10 +234,10 @@ def HGPfunc(x,y,plot):
         for i in range(numrestarts):    
             #k1is4 = np.random.uniform(1e-2,1e3)
             #k2is4 = np.random.uniform(1e-1,1e3)
-            k1is4 = np.random.uniform(1e-2,1e3)
-            k2is4 = np.random.uniform(1e-2,1e3)
+            k1is4 = np.random.uniform(hlow,hhigh)
+            k2is4 = np.random.uniform(hlow,hhigh)
             kis4 = np.ndarray((numh,), buffer=np.array([k1is4,k2is4]), dtype = float)
-            s4res = minimize(lmlh,kis4,args=(y,R),method = 'L-BFGS-B',jac=lmlgh,bounds = ((1e-2,1e3),(1e-2,1e3)),options={'maxiter':1e2})
+            s4res = minimize(lmlh,kis4,args=(y,R),method = 'L-BFGS-B',jac=lmlgh,bounds = ((hlow,hhigh),(hlow,hhigh)),options={'maxiter':1e2})
             step4res = []
             if s4res.success:
                 step4res.append(s4res.x)
@@ -155,8 +249,8 @@ def HGPfunc(x,y,plot):
                 #raise ValueError(s4res.message)
                 #k1is4 = np.random.uniform(1e-2,1e3)
                 #k2is4 = np.random.uniform(2e-1,1e3)
-                k1is4 = np.random.uniform(1e-2,1e3)
-                k2is4 = np.random.uniform(1e-2,1e3)
+                k1is4 = np.random.uniform(hlow,hhigh)
+                k2is4 = np.random.uniform(hlow,hhigh)
                 print("error in hypopth() - reinitialising hyperparameters")
                 continue 
             k1s4[i] = step4res[0][0]
@@ -182,8 +276,8 @@ def HGPfunc(x,y,plot):
             #k1is4,k2is4  = np.random.uniform(1e-2,1e2,2)
             #k1is3, k1is4  =  np.random.uniform(1e-2,1e2,2)
             #k2is3, k2is4  =  np.random.uniform(1e-1,1e2,2)
-            k1is3  =  np.random.uniform(1e-2,1e3,1)
-            k2is3  =  np.random.uniform(1e-2,1e3,1)
+            k1is3  =  np.random.uniform(hlow,hhigh,1)
+            k2is3  =  np.random.uniform(hlow,hhigh,1)
             z = np.empty([n,1])
             for j in range(n):
                 #np.random.seed()
@@ -197,7 +291,7 @@ def HGPfunc(x,y,plot):
                 
                 continue
             #  Step 3: estimate GP2 on D' - (x,z)
-            kernel2 = C(k1is3, (1e-2, 1e3)) * RBF(k2is3, (1e-2, 1e3)) 
+            kernel2 = C(k1is3, (hlow,hhigh)) * RBF(k2is3, (hlow,hhigh)) 
             gpr2 = GaussianProcessRegressor(kernel=kernel2, n_restarts_optimizer = numrestarts, normalize_y=False, alpha=np.var(z))
             
             gpr2.fit(x, z)
@@ -234,7 +328,7 @@ def HGPfunc(x,y,plot):
         return fmstf,varfmstf, lmloptf, MSE, rf, NLPD #  , NLPD 
     #kernel1 = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-3, 1e3)) + W(1.0, (1e-5, 1e5))
     #gpr1 = GaussianProcessRegressor(kernel=kernel1, n_restarts_optimizer = 0, normalize_y=True)
-    kernel1 = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-3, 1e3)) 
+    kernel1 = C(1.0, (hlow,hhigh)) * RBF(1.0, (hlow,hhigh)) 
     gpr1 = GaussianProcessRegressor(kernel=kernel1, n_restarts_optimizer = 10, normalize_y=False, alpha=np.var(y))
     gpr1.fit(x, y)
     ystar1, sigma1 = gpr1.predict(x, return_std=True )
@@ -242,7 +336,7 @@ def HGPfunc(x,y,plot):
     #sigma1 = np.reshape(sigma1,(np.size(sigma1), 1))
 
     numiters = 20
-    numrestarts = 5
+    numrestarts = 10
     start_time = time.time()
     fmstf,varfmstf, lmlopt, mse, _,NLPD = hetloopSK(ystar1,var1,numiters,numrestarts)
     duration = time.time() - start_time
@@ -339,12 +433,14 @@ lml = np.empty([numedges,numiters])
 MSE = np.empty([numedges,numiters])
 NLPD = np.empty([numedges,numiters])
 
-for i in range(np.size(snr,0)):
+""" for i in range(np.size(snr,0)):
     prmn[i], prmnp[i], lmls, MSEs, NLPDs = HGPfunc(x,snr[i],False)
     lml[i] = lmls.reshape(numiters)
     MSE[i] = MSEs.reshape(numiters)
-    NLPD[i] = NLPDs.reshape(numiters)
-#prmn, prmnp, lml, MSE, NLPD = HGPfunc(x,snr,False)
+    NLPD[i] = NLPDs.reshape(numiters) """
+hlow = 1e-4
+hhigh = 1e4
+prmn, prmnp, lml, MSE, NLPD = HGPfunc(x,snr,False, hlow, hhigh)
 sig = (prmnp - prmn)
 
 prmnp1 = prmn + sig    
@@ -360,7 +456,7 @@ prmnn5 = prmn - 5*sig
 
 
 # %%
-algtest = False
+algtest = True
 if algtest:
 
     font = { 'family' : 'sans-serif',
@@ -395,50 +491,57 @@ if algtest:
                             (prmnn5)[::-1]]),
             alpha=0.3, fc='g', ec='None')
 
-    ax2.plot(x, sig, '--', label="learned $\sigma$")
-    ax2.plot(x, sd, '-', label="true $\sigma$")
-    ax2.set_ylabel("$\sigma$")
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
+    #ax2.plot(x, sig, '--', label="learned $\sigma$")
+    #ax2.plot(x, sd, '-', label="true $\sigma$")
+    #ax2.set_ylabel("$\sigma$")
+    ax.set_xlabel("Time (AU)")
+    ax.set_ylabel("Q-factor (dB)")
     ax.set_xlim([x[0], x[-1]])
     #ax.set_ylim([6.4, 9.5])
     #ax.set_xticklabels(xlab)
     #ax.set_yticklabels(ylab)
-    plt.legend(ncol=1)
+    ax.legend(ncol=1)
     #plt.savefig('HGPtestgoldberg.pdf', dpi=200,bbox_inches='tight')
-    plt.savefig('HGPtestwilliams.pdf', dpi=200,bbox_inches='tight')
+    #plt.savefig('HGPtestwilliams.pdf', dpi=200,bbox_inches='tight')
     #plt.savefig('HGPtestyuanwhaba.pdf', dpi=200,bbox_inches='tight')
+    plt.savefig('hgpfitMSQ.pdf', dpi=200,bbox_inches='tight')
+    plt.show()
+
+    f, ax = plt.subplots()
+    ax.plot(x, sig, '--', label="learned $\sigma$")
+    #ax.plot(x, sd, '-', label="true $\sigma$")
+    ax.set_xlabel("Time (AU)")
+    ax.set_ylabel("Q-factor $\sigma$ (dB)")
+    #plt.savefig('HGPtestgoldbergsig.pdf', dpi=200,bbox_inches='tight')
+    plt.savefig('hgpfitMSQsig.pdf', dpi=200,bbox_inches='tight')
     plt.show()
 
     plt.plot(lml)
     plt.ylabel("LML")
+    plt.xlabel("Number of iterations")
     #plt.savefig('HGPtestgoldberglml.pdf', dpi=200,bbox_inches='tight')
-    plt.savefig('HGPtestwilliamslml.pdf', dpi=200,bbox_inches='tight')
+    #plt.savefig('HGPtestwilliamslml.pdf', dpi=200,bbox_inches='tight')
     #plt.savefig('HGPtestyuanwhabalml.pdf', dpi=200,bbox_inches='tight')
+    plt.savefig('lmlconvergence.pdf', dpi=200,bbox_inches='tight')
     plt.show()
 
     plt.plot(MSE)
     plt.ylabel("MSE")
+    plt.xlabel("Number of iterations")
     #plt.savefig('HGPtestgoldbergmse.pdf', dpi=200,bbox_inches='tight')
-    plt.savefig('HGPtestwilliamsmse.pdf', dpi=200,bbox_inches='tight')
+    #plt.savefig('HGPtestwilliamsmse.pdf', dpi=200,bbox_inches='tight')
     #plt.savefig('HGPtestyuanwhabamse.pdf', dpi=200,bbox_inches='tight')
-
+    plt.savefig('mseconvergence.pdf', dpi=200,bbox_inches='tight')
     plt.show()
 
-    plt.plot(NLPD)
+    """ plt.plot(NLPD)
     plt.ylabel("NLPD")
     #plt.savefig('HGPtestgoldbergnlpd.pdf', dpi=200,bbox_inches='tight')
-    plt.savefig('HGPtestgoldbergnlpd.pdf', dpi=200,bbox_inches='tight')
+    #plt.savefig('HGPtestgoldbergnlpd.pdf', dpi=200,bbox_inches='tight')
     #plt.savefig('HGPtestyuanwhabanlpd.pdf', dpi=200,bbox_inches='tight')
-    plt.show()
-
-    """ f, ax = plt.subplots()
-    ax.plot(x, sig, '--', label="learned $\sigma$")
-    ax.plot(x, sd, '-', label="true $\sigma$")
-    ax.set_xlabel("x")
-    ax.set_ylabel("$\sigma$")
-    plt.savefig('HGPtestgoldbergsig.pdf', dpi=200,bbox_inches='tight')
     plt.show() """
+
+    
 
 
 # %%
@@ -486,7 +589,7 @@ ax.set_xlim([x[0], x[-1]])
 #ax.set_yticklabels(ylab)
 #plt.axis([-1,100,1.0,8.0])
 plt.legend()
-#plt.savefig('JOCNhetGP.pdf', dpi=200,bbox_inches='tight')
+plt.savefig('hgpfitMSQ.pdf', dpi=200,bbox_inches='tight')
 #plt.savefig('hetGPextdegst.pdf', dpi=200,bbox_inches='tight')
 #plt.savefig('hetGPextdegstmoredata.pdf', dpi=200,bbox_inches='tight')
 #plt.savefig('hetGPextdegdvmoredata.pdf', dpi=200,bbox_inches='tight')
